11c90a400e2e3841b799ef6983b2b120b33e8e09
indhu <indhu100407@gmail.com>
2026-01-30 20:55:29 +0530

    problem 44 solved

--- problem_44.py
+++ problem_44.py
@@ -3,8 +3,8 @@
 
 def fibonacci(n):
     fib = [0, 1]
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
     return fib
 
 print(f"First 10 Fibonacci numbers: {fibonacci(10)}")
