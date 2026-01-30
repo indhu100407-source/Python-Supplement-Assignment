0e9f9391c47b71ac9781bdd98870b8945ae92a8d
indhu <indhu100407@gmail.com>
2026-01-30 21:20:06 +0530

    problem 53 solved

--- problem_53.py
+++ problem_53.py
@@ -1,9 +1,9 @@
 # Problem 53: Check if two strings are anagrams
 # Find and fix the error
 
-def are_anagrams(str1, str2):
-    return sorted(str1) == sorted(str2)
+def are_anagrams (str1, str2):
+    return sorted(str1.lower()) == sorted(str2.lower())
 
 word1 = "Listen"
 word2 = "Silent"
-print(f"Are anagrams: {are_anagrams(word1, word2)}")
+print(f"Are anagrams: {are_anagrams (word1, word2)}")
