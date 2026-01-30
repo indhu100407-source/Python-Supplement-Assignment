9d4f0329a5fc39d24ce525bea1e1fbbee62caeec
indhu <indhu100407@gmail.com>
2026-01-30 19:58:31 +0530

    problem 29 solved

--- problem_29.py
+++ problem_29.py
@@ -2,9 +2,9 @@
 # Find and fix the error
 
 def add_to_list(item, lst=[]):
+    if lst is None:
+        lst = []
     lst.append(item)
     return lst
 
-print(add_to_list(1))
-print(add_to_list(2))
-print(add_to_list(3))
+
